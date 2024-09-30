a601ca3824a638d5c415d55c50292939a92be726
Emily Tuttle <emilytuttle00@gmail.com>
2024-09-30 00:45:33 -0600

    db names fixed

--- controllers/users.js
+++ controllers/users.js
@@ -4,8 +4,9 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res) => {
     const result = await mongodb.getDatabase().db().collection('users').find();
     result.toArray().then((users) => {
-        res.setHeader('Content-Type: application/json');
-        res.status(200),json(users);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+        console.log(users)
     })
     .catch((err) => {
         console.log(err)
@@ -16,8 +17,8 @@ const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
     result.toArray().then((users) => {
-        res.setHeader('Content-Type: application/json');
-        res.status(200),json(users[0]);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users[0]);
     })
     .catch((err) => {
         console.log(err)
