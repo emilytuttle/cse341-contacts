e9d129e3cee971beb14d8e9feff2a36fdcb24e3c
Emily Tuttle <emilytuttle00@gmail.com>
2024-10-09 03:01:53 -0600

    swagger added

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+    //#swagger.tags=["Users"]
     const result = await mongodb.getDatabase().db().collection('users').find();
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
@@ -13,6 +14,7 @@ const getAll = async (req, res) => {
 
 };
 const getSingle = async (req, res) => {
+    //#swagger.tags=["Users"]
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
     result.toArray().then((users) => {
@@ -25,6 +27,7 @@ const getSingle = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
+    //#swagger.tags=["Users"]
     const user = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -41,6 +44,7 @@ const createUser = async (req, res) => {
 }
 
 const updateUser = async (req, res) => {
+    //#swagger.tags=["Users"]
     const userId = new ObjectId(req.params.id)
     const user = {
         firstName: req.body.firstName,
@@ -58,6 +62,7 @@ const updateUser = async (req, res) => {
 }
 
 const deleteUser = async (req, res) => {
+    //#swagger.tags=["Users"]
     const userId = new ObjectId(req.params.id)
     const user = {
         firstName: req.body.firstName,
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const routes = require('express').Router();
 const dataDisplay = require('../controllers/');
 
+routes.use('/', require('./swagger'))
+
+routes.get('/', (req, res) => {
+    //#swagger.tags=['Hello World']
+    res.send('Hello World');
+})
+
 routes.get('/', dataDisplay.dataDisplay);
 routes.use('/users', require('./users'));
 
